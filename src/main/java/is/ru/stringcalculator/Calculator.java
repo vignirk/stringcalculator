a719bcc47fd308672a885aff66331ce9015b4185
Hannes Petursson <hannes@gagnavarslan.is>
2014-10-11 11:20:39 +0000

    Refactor code

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,16 +3,12 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		if (text.equals(""))
+		if(text.equals("")){
 			return 0;
+		}
 		else if(text.contains(",")){
-        	String[] numbers = text.split(",");
-        	int total = 0;
-        	for(String number : numbers){
-		    	total += toInt(number);
-			}
-			return total;
- 		}
+			return sum(splitNumbers(text));
+		}
 		else
 			return 1;
 	}
@@ -20,6 +16,19 @@ public class Calculator {
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
+
+	private static String[] splitNumbers(String numbers){
+	    return numbers.split(",");
+	}
+      
+    private static int sum(String[] numbers){
+ 	    int total = 0;
+        for(String number : numbers){
+		    total += toInt(number);
+		}
+		return total;
+    }
+
 
 
 }
