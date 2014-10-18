1fdf800f279fa2dc2bd082f6db0e16797c0204d8
vignirk <vignirk@gmail.com>
2014-10-18 14:57:31 +0000

    Lidur 3 og newLine test fall

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
+		else if(text.contains(",") || text.contains("\\n")){
 			return sum(splitNumbers(text));
 		}
 		else
@@ -18,7 +18,7 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split(",");
+	    return numbers.split(",|\\n");
 	}
       
     private static int sum(String[] numbers){
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -25,8 +25,13 @@ public class CalculatorTest {
 	}	
 
 	@Test
-    public void testMultipleNumbers(){
+    	public void testMultipleNumbers(){
     	assertEquals(6, Calculator.add("1,2,3"));
-    }
+	}
+
+	 @Test
+    	public void testNewLine(){
+        assertEquals(6, Calculator.add("1\n2,3"));
+    	}
 
 }
